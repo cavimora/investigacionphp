c8b7fab803c4c024865b5dcac9adb3d6dd43cacc
cavimora <cavimora@gmail.com>
2017-03-27 14:39:58 -0600

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,3 @@
+	<script type="text/javascript" src="assets/js/main.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,6 @@
 <head>
 	<title>Investigacion PHP</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
-	<script type="text/javascript" src="assets/js/main.js"></script>
 </head>
 <body>
 
