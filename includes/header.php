4676c2c2ef198c3a00b115248b5a947728c78a05
cavimora <cavimora@gmail.com>
2017-03-27 14:20:32 -0600

    commit

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<title>Investigacion PHP</title>
-	<link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
-	<script type="text/javascript" src="/assets/js/main.js"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<script type="text/javascript" src="assets/js/main.js"></script>
 </head>
 <body>
 
