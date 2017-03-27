e791312a3305545700f08ff3640ae551fee9c3e5
cavimora <cavimora@gmail.com>
2017-03-27 14:34:37 -0600

    commit

--- classes/ClassTrabajo.php
+++ classes/ClassTrabajo.php
@@ -3,11 +3,14 @@
 
 class Trabajo {
     function __construct() {
-        echo 'Se acaba de instanciar la clase Trabajo';
+        echo '<h2>Se acaba de instanciar la clase Trabajo</h2>';
+        echo '<br>';
     }
 
     public function Mostrar($mostrar){
     	echo $mostrar;
+    	
+    	echo '<br>';
     }
 }
 
--- index.php
+++ index.php
@@ -10,8 +10,42 @@ CONTENIDO
 echo "<h1>Investigacion PHP</h1>";
 
 $trabajo = new Trabajo();
-$trabajo->mostrar("PRUEBA");
+$trabajo->mostrar("Ejemplo Metodo POST");
+if(empty($_POST['name'])) {
+	?>
 
+	<form action="index.php" method="post">
+		Nombre: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		<input type="submit">
+	</form>
+
+	<?
+} else{
+	?>
+		Bienvenido <?php echo $_POST["name"]; ?><br>
+		Su e-mail es: <?php echo $_POST["email"]; ?>	
+	<?
+}
+
+
+$trabajo->mostrar("Ejemplo Metodo GET");
+if(empty($_GET)) {
+	?>
+
+	<form action="index.php" method="GET">
+		Nombre: <input type="text" name="name"><br>
+		E-mail: <input type="text" name="email"><br>
+		<input type="submit">
+	</form>
+
+	<?
+} else{
+	?>
+		Bienvenido <?php echo $_GET["name"]; ?><br>
+		Su e-mail es: <?php echo $_GET["email"]; ?>	
+	<?
+}
 /*
 FINALIZAR CONTENIDO
 */
@@ -19,3 +53,4 @@ FINALIZAR CONTENIDO
 include 'includes/footer.php';
 
 ?>
+
