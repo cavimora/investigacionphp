10e9093af907c6b1632a32de3eab15438f5d9224
cavimora <cavimora@gmail.com>
2017-03-27 14:15:18 -0600

    footer and header structure added to the index

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Investigacion PHP</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
+	<script type="text/javascript" src="/assets/js/main.js"></script>
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'includes/header.php';
+
+/*
+CONTENIDO
+*/
+
+echo "<h1>Investigacion PHP</h1>";
+
+/*
+FINALIZAR CONTENIDO
+*/
+
+include 'includes/footer.php';
+
+?>
